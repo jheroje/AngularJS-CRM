e84e6ea9070fc66ff070fa2c06439acb02cc39fb
Javier <javierho93@hotmail.com>
2017-05-08 12:37:23 +0100

    CRUD done

--- src/main.js
+++ src/main.js
@@ -8,10 +8,46 @@
   MainController.inject = ['$scope'];
 
   function MainController($scope) {
+    $scope.users = [];
+    $scope.new = {};
+    $scope.temp = {};
+    $scope.create = create;
+    $scope.reset = reset;
+    $scope.edit = edit;
+    $scope.modify = modify;
+    $scope.remove = remove;
+    $scope.editing = false;
     activate();
 
     ////////////////
 
     function activate() {}
+
+    function create(user) {
+      $scope.users.push(user);
+      reset();
+    }
+
+    function reset() {
+      if ($scope.editing) {
+        $scope.editing = false;
+      }
+      $scope.new = {};
+    }
+
+    function edit(user) {
+      $scope.new = angular.copy(user);
+      $scope.temp = user;
+      $scope.editing = true;
+    }
+
+    function modify(user) {
+      remove($scope.temp);
+      create(user);
+    }
+
+    function remove(user) {
+      $scope.users = $scope.users.filter((el) => (el !== user));
+    }
   }
 }());
